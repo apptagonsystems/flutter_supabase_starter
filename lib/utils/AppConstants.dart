a93fc885a4094ca7999e3d088a12bb3f32f93d94
apptagon.systems <apptagon.systems@gmail.com>
2023-01-12 14:17:59 +0000

    second  commit

--- lib/utils/AppConstants.dart
+++ lib/utils/AppConstants.dart
@@ -1,9 +1,9 @@
 class AppConstants {
-  static const String kAppName = "ALL IN ONE";
+  static const String kAppName = "FLUTTER SUPABASE STARTER PROJECT";
   // SUPABASE
-  static const String supabaseURL = 'https://nlirepdwidsjmiqqkhxb.supabase.co';
+  static const String supabaseURL = '';
   static const String supabaseAnonKey =
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5saXJlcGR3aWRzam1pcXFraHhiIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzMzNTI3MzMsImV4cCI6MTk4ODkyODczM30.2Z0L144_bs5uAV0mmxAIQ3sIiT1pRfrvAng5iQwaEs0';
+      '';
 
 // ACCOUNT STATUS
   static const String accountStatusAPPROVED = "APPROVED";
